eb62ec37a59a5a1cdc15f6472a5874df14f94d12
Ashish Raghuvanshi <thetypo36@gmail.com>
2025-08-18 23:44:07 +0530

    prefixSum:subarray_equal_k

---
+++ prefix_sum/Subarray_sum_equals_k.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int subarraySum(vector<int> &nums, int k)
+{
+    int n = nums.size();
+    vector<int> prefixSum(n + 1, 0);
+
+    for (int i = 0; i < n; i++)
+    {
+        prefixSum[i + 1] = prefixSum[i] + nums[i];
+        cout << prefixSum[i] << endl;
+    }
+    cout << prefixSum[n] << endl;
+    int count = 0;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i; j < n; j++)
+        {
+            if (prefixSum[j + 1] - prefixSum[i] == k)
+            {
+                count++;
+            }
+        }
+    }
+
+    return count;
+}
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> arr(n, 0);
+    for (auto &i : arr)
+    {
+        cin >> i;
+    }
+    int k;
+    cin >> k;
+    cout << subarraySum(arr, k) << endl;
+}
